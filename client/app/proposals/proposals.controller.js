9423d75439829e6d97b0b188b5f821b38099838d
GPad <gpadovani@gmail.com>
2015-11-04 22:30:53 +0100

    Ability to create new proposal WIP

--- client/app/proposals/proposals.controller.js
+++ client/app/proposals/proposals.controller.js
@@ -1,6 +1,36 @@
 'use strict';
 
 angular.module('lambdaConApp')
-  .controller('ProposalsCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('ProposalsCtrl', function ($scope, Auth) {
+
+    $scope.isLoggedIn = Auth.isLoggedIn;
+    $scope.isAdmin = Auth.isAdmin;
+    $scope.getCurrentUser = Auth.getCurrentUser;
+
+    $scope.createProposal = function(form) {
+      $scope.submitted = true;
+
+      if(form.$valid) {
+        Proposal.create({
+          title: $scope.proposal.title,
+          description: $scope.proposal.description
+        })
+        .then( function() {
+          // Account created, redirect to home
+          console.log("OK!!!!");
+        })
+        .catch( function(err) {
+          err = err.data;
+          $scope.errors = {};
+
+          // Update validity of form fields that match the mongoose errors
+          angular.forEach(err.errors, function(error, field) {
+            form[field].$setValidity('mongoose', false);
+            $scope.errors[field] = error.message;
+          });
+        });
+      }
+    };
+
+
   });
